a6323e01453cf52ae26333eac3ca4a9cec9d17a5
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-16 14:31:52 +0700

    [REFACTOR] Fix NotificationService Struct

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -7,8 +7,7 @@ use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
 
-pub struct NotificationService{
-}
+pub struct NotificationService;
 
 impl NotificationService {
     pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
